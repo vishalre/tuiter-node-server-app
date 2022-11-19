8b23caf2fcf4de8217dc746219ae7c5312e4cb02
Vishal Reddy Gurrala <gurrala.v@northeastern.edu>
2022-11-19 17:10:58 -0600

    a9 - using mongodbatlas cluster

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+// To run MongoDB - mongod --config /opt/homebrew/etc/mongod.conf
+
 import express from 'express'
+import mongoose from "mongoose";
 import cors from 'cors'
 import HelloController
     from "./controllers/hello-controller.js"
@@ -7,6 +10,9 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+                          || "mongodb://localhost:27017/Tuiter";
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json());
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,10 +1,7 @@
-import posts from "./tuits.js";
+import * as tuitsDao from './tuits-dao.js'
 
-let tuits = posts;
-
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
     const newTuit = req.body;
-    newTuit._id = (new Date()).getTime() + '';
     newTuit.likes = 0;
     newTuit.liked = false;
     newTuit.dislikes = 0;
@@ -17,28 +14,31 @@ const createTuit = (req, res) => {
     newTuit.time = "Just now";
     newTuit.image =
         "https://www.pngfind.com/pngs/m/299-2995596_nasa-logo-photo-nasa-logo-1-1-hd.png";
-    tuits.push(newTuit);
-    res.json(newTuit);
+    const insertedTuit = await tuitsDao
+        .createTuit(newTuit);
+    res.json(insertedTuit);
 }
 
-const findTuits = (req, res) =>
+const findTuits = async (req, res) => {
+    const tuits = await tuitsDao.findTuits()
     res.json(tuits);
+}
 
-const updateTuit = (req, res) => {
+
+const updateTuit = async (req, res) => {
     const tuitdIdToUpdate = req.params['tid'];
     const updates = req.body;
-    const tuitIndex = tuits.findIndex(
-        (t) => t._id === tuitdIdToUpdate)
-    tuits[tuitIndex] =
-        {...tuits[tuitIndex], ...updates};
-    res.sendStatus(200);
+    const status = await tuitsDao
+        .updateTuit(tuitdIdToUpdate,
+                    updates);
+    res.json(status);
 }
 
-const deleteTuit = (req, res) => {
-    const tuitdIdToDelete = req.params['tid'];
-    tuits = tuits.filter(t =>
-                             t._id !== tuitdIdToDelete);
-    res.sendStatus(200);
+const deleteTuit = async (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    const status = await tuitsDao
+        .deleteTuit(tuitdIdToDelete);
+    res.json(status);
 }
 
 export default (app) => {
