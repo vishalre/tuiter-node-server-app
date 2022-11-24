5cd98d54e3e1118e7bbe5c29f63fa2bb0b6210eb
Vishal Reddy Gurrala <gurrala.v@northeastern.edu>
2022-11-23 18:11:10 -0600

    Modified schema a9

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,16 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
-                                   tuit: String,
-                                   likes: Number,
-                                   liked: Boolean,
-                               }, {collection: 'tuits'});
+    topic: String,
+    username: String,
+    handle: String,
+    time: String,
+    image: String,
+    title: String,
+    tuit: String,
+    likes: Number,
+    dislikes: Number,
+    replies: Number,
+    retuits: Number,
+    liked: Boolean,
+    }, {collection: 'tuits'});
 export default schema;
